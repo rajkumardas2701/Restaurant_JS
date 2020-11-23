ae2f53ab345f6780b6f53f1e9d5b6d4b5874d8ea
rajkumardas2701 <41339878+rajkumardas2701@users.noreply.github.com>
2020-11-23 15:44:12 +0530

    adding navbar with elements

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-plusplus, no-return-assign, no-use-before-define,
+import/extensions, implicit-arrow-linebreak, operator-linebreak  */
+import navBar from './navbar.js';
+
+const component = () => {
+  navBar();
+};
+
+component();
---
+++ src/navbar.js
@@ -0,0 +1,57 @@
+const navBar = () => {
+  const content = document.getElementById('content');
+  content.setAttribute('class', 'container');
+
+  const navSection = document.createElement('div');
+  content.appendChild(navSection);
+  navSection.setAttribute('class', 'nav-section');
+
+  const navItems = document.createElement('div');
+  navSection.appendChild(navItems);
+  navItems.setAttribute('class', 'nav-items');
+
+  const mainLogo = document.createElement('div');
+  navItems.appendChild(mainLogo);
+  mainLogo.setAttribute('class', 'main-logo');
+
+  const logoText = document.createElement('a');
+  logoText.innerHTML = 'The Global Cuisine';
+  mainLogo.appendChild(logoText);
+
+  const navRightSide = document.createElement('div');
+  navRightSide.setAttribute('class', 'nav-Right-Side');
+  navItems.appendChild(navRightSide);
+
+  const navTabs = () => {
+    const navTabItems = [
+      {
+        className: 'Menu',
+        contentID: 'menuTab',
+        contentText: 'Menu',
+      },
+      {
+        className: 'About',
+        contentID: 'aboutTab',
+        contentText: 'About',
+      },
+      {
+        className: 'Contact',
+        contentID: 'contactTab',
+        contentText: 'Contact',
+      },
+    ];
+
+    for (let i = 0; i < navTabItems.length; i += 1) {
+      const navRightTab = document.createElement('div');
+      navRightTab.setAttribute('class', `${navTabItems[i].className}`);
+      const navRightTabsText = document.createElement('a');
+      navRightTabsText.setAttribute('id', `${navTabItems[i].contentID}`);
+      navRightTabsText.innerHTML = `${navTabItems[i].contentText}`;
+      navRightTab.appendChild(navRightTabsText);
+      navRightSide.appendChild(navRightTab);
+    }
+  };
+  navTabs();
+};
+
+export default navBar;
